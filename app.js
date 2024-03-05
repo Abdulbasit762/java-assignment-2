065e631656c6e5b196c6e971a3937ee17dfa45e5
Abdulbasit762 <154410960+Abdulbasit762@users.noreply.github.com>
2024-03-06 03:48:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+/////       assignment no 2
+var username ;
+
+var myname ="Abdul basit siddiqui"
+
+var message = "Hellow world"
+
+var age = "17 years old"
+
+var corsename ="Certified mobile Application Development"
+
+var exp ="PIZZA \n PIZZ \n PIZ \n PI \n P"
+
+var email =" My email adderss is:itaha4361@gmail.com"
+
+var Jsbook ="I am trying to learn from the Book A smarter way to learn JavaScript  "
+
+alert(message)
+
+alert(myname)
+
+alert(age)
+
+alert(corsename)
+
+alert(exp)
+
+alert(email)
+
+alert(Jsbook)
+
+document.write("Yah! I can write HTML. content through Javascript")
+
+var desin = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert(desin)
